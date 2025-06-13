44aa363aa00181d9e1de17a7c81a84141a49746a
MiquelMarinoEspinosa <miquel.marino.espinosa@gmail.com>
2025-06-13 10:20:21 +0000

    display ingredients

--- foodapp/src/components/FoodDetails.jsx
+++ foodapp/src/components/FoodDetails.jsx
@@ -49,7 +49,24 @@ export default function FoodDetails({ foodId }) {
             <strong>{food.pricePerServing / 100} Per serving</strong>
           </span>
         </div>
-
+        <h2>Ingredients</h2>
+        <div>
+          {food.extendedIngredients.map((item) => (
+            <div>
+              <img
+                src={
+                  `https://spoonacular.com/cdn/ingredients_100x100/` +
+                  item.image
+                }
+                alt=""
+              />
+              <h3>{item.name}</h3>
+              <h3>
+                {item.amount} {item.unit}
+              </h3>
+            </div>
+          ))}
+        </div>
         <h2>Instructions</h2>
         <div className={styles.recipeInstructions}>
           <ol>
