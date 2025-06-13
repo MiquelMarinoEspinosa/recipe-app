d204b7ad40ba19a00d4add76db9f329eef5701fc
MiquelMarinoEspinosa <miquel.marino.espinosa@gmail.com>
2025-06-13 08:15:46 +0000

    fetch food details

--- foodapp/src/components/FoodDetails.jsx
+++ foodapp/src/components/FoodDetails.jsx
@@ -1,3 +1,29 @@
+import { useEffect, useState } from "react";
+
+const URL = import.meta.env.VITE_API_URL;
+const API_KEY = import.meta.env.VITE_API_KEY;
+
 export default function FoodDetails({ foodId }) {
-  return <div>Food Details {foodId}</div>;
+  const [food, setFood] = useState({});
+  useEffect(() => {
+    async function fetchFood() {
+      if (!foodId) {
+        return;
+      }
+      const response = await fetch(
+        `${URL}/${foodId}/information?apiKey=${API_KEY}`
+      );
+      const data = await response.json();
+      console.log(data);
+      setFood(data);
+    }
+    fetchFood();
+  }, [foodId]);
+  return (
+    <div>
+      Food Details {foodId}
+      {food.title}
+      <img src={food.image} alt="" />
+    </div>
+  );
 }
