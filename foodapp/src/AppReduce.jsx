331c32e0f9e2e70c8da7e8c655029b6f2402cb56
MiquelMarinoEspinosa <miquel.marino.espinosa@gmail.com>
2025-06-16 09:40:29 +0000

    object reducer

--- foodapp/src/AppReduce.jsx
+++ foodapp/src/AppReduce.jsx
@@ -1,19 +1,19 @@
 import { useReducer, useState } from "react";
 
 export default function AppReduce() {
-  const [state, dispatch] = useReducer(reducer, 0);
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
   function reducer(state, action) {
     if (action.type === "increment") {
-      return state + action.payload;
+      return { ...state, count: state.count + action.payload };
     }
 
     if (action.type === "decrement") {
-      return state - action.payload;
+      return { ...state, count: state.count - action.payload };
     }
   }
   return (
     <div>
-      Use reducer {state}
+      Use reducer {state.count}
       <button onClick={() => dispatch({ type: "increment", payload: 1 })}>
         Increment count
       </button>
