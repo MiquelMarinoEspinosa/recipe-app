0913e89a487bffe01b3cc7b50938b042566943b7
MiquelMarinoEspinosa <miquel.marino.espinosa@gmail.com>
2025-06-16 10:13:58 +0000

    bank account reducer

--- foodapp/src/main.jsx
+++ foodapp/src/main.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import AppReduce from "./AppReduce";
+import AppBankAccountReducer from "./AppBankAccountReducer";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <App /> */}
-    <AppReduce />
+    {/* <AppReduce /> */}
+    <AppBankAccountReducer />
   </React.StrictMode>
 );
