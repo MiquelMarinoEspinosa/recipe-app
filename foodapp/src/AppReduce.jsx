c709db8d7b1e621216e31170f51ab38013bf287b
MiquelMarinoEspinosa <miquel.marino.espinosa@gmail.com>
2025-06-16 09:52:32 +0000

    complex state

--- foodapp/src/AppReduce.jsx
+++ foodapp/src/AppReduce.jsx
@@ -1,23 +1,34 @@
 import { useReducer, useState } from "react";
 
 export default function AppReduce() {
-  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  const [state, dispatch] = useReducer(reducer, { count: 0, incrementBy: 1 });
   function reducer(state, action) {
     if (action.type === "increment") {
-      return { ...state, count: state.count + action.payload };
+      return { ...state, count: state.count + state.incrementBy };
     }
 
     if (action.type === "decrement") {
-      return { ...state, count: state.count - action.payload };
+      return { ...state, count: state.count - state.incrementBy };
+    }
+
+    if (action.type === "setIncrement") {
+      return { ...state, incrementBy: action.payload };
     }
   }
   return (
     <div>
       Use reducer {state.count}
-      <button onClick={() => dispatch({ type: "increment", payload: 1 })}>
+      <input
+        type="text"
+        value={state.incrementBy}
+        onChange={(e) =>
+          dispatch({ type: "setIncrement", payload: Number(e.target.value) })
+        }
+      />
+      <button onClick={() => dispatch({ type: "increment" })}>
         Increment count
       </button>
-      <button onClick={() => dispatch({ type: "decrement", payload: 1 })}>
+      <button onClick={() => dispatch({ type: "decrement" })}>
         Decrement count
       </button>
     </div>
